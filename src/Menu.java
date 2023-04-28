aa25b11136cc0c309346e59db8bee12b89a334d4
JeanCarlo2709 <jeanaraya27@estudiantec.cr>
2023-04-28 15:49:34 -0600

    Ajuste de clase Menu

--- src/Menu.java
+++ src/Menu.java
@@ -3,9 +3,9 @@ import javax.swing.*;
 public class Menu extends javax.swing.JFrame{
     public javax.swing.JLabel Titulo;
     public javax.swing.JButton Exit;
-    public javax.swing.JLabel añadirUserLabel;
-    public javax.swing.JComboBox<String> userañdirComboBox;
-    public javax.swing.JButton añadirButton;
+    public javax.swing.JLabel TituloPlatillo;
+    public javax.swing.JComboBox<String> Eleccion;
+    public javax.swing.JButton PedidoHecho;
     public Menu()
     {
         initComponents();
@@ -18,9 +18,9 @@ public class Menu extends javax.swing.JFrame{
 
         Titulo = new javax.swing.JLabel();
         Exit = new javax.swing.JButton();
-        añadirUserLabel = new javax.swing.JLabel();
-        userañdirComboBox = new javax.swing.JComboBox<>();
-        añadirButton = new javax.swing.JButton();
+        TituloPlatillo = new javax.swing.JLabel();
+        Eleccion = new javax.swing.JComboBox<>();
+        PedidoHecho = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -28,20 +28,20 @@ public class Menu extends javax.swing.JFrame{
         Titulo.setForeground(new java.awt.Color(0, 60, 255));
         Titulo.setText("Seleccione su platillo");
 
-        añadirUserLabel.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12));
-        añadirUserLabel.setText("Nombre del platillo que desea pedir");
+        TituloPlatillo.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12));
+        TituloPlatillo.setText("Nombre del platillo que desea pedir");
 
-        userañdirComboBox.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12));
-        userañdirComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        userañdirComboBox.addActionListener(new java.awt.event.ActionListener() {
+        Eleccion.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12));
+        Eleccion.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        Eleccion.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
 
             }
         });
 
-        añadirButton.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12));
-        añadirButton.setText("Concretar pedido");
-        añadirButton.addActionListener(new java.awt.event.ActionListener() {
+        PedidoHecho.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12));
+        PedidoHecho.setText("Concretar pedido");
+        PedidoHecho.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
 
             }
@@ -63,13 +63,13 @@ public class Menu extends javax.swing.JFrame{
                                 .addContainerGap(150, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                                .addComponent(añadirButton)
-                                                .addGap(251, 251, 251))
+                                                .addComponent(PedidoHecho)
+                                                .addGap(440, 440, 440))
                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                                                .addComponent(añadirUserLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                                .addComponent(userañdirComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(TituloPlatillo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(Eleccion, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                                                 ))
                                                 .addGap(146, 146, 146))))
                         .addGroup(layout.createSequentialGroup()
@@ -89,11 +89,11 @@ public class Menu extends javax.swing.JFrame{
                                         .addComponent(Titulo)
                                         .addComponent(Exit))
                                 .addGap(91, 91, 91)
-                                .addComponent(añadirUserLabel)
+                                .addComponent(TituloPlatillo)
                                 .addGap(39, 39, 39)
-                                .addComponent(userañdirComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(Eleccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(28, 28, 28)
-                                .addComponent(añadirButton)
+                                .addComponent(PedidoHecho)
                                 .addGap(29, 29, 29)
                                 .addContainerGap(58, Short.MAX_VALUE))
         );

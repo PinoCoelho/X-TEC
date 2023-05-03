ae99caaecf1de43ecb06a39845e7e1f201107244
josumveg <josepabloumana@gmail.com>
2023-05-03 02:55:59 -0600

    XML implementados

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import javax.xml.transform.TransformerException;
+
 public class Main {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) throws Exception {
+
+        ServerApp.iniciarXML();
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
